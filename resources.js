5e21471ecdbfb5461e8696803d0d90c859a3fdbe
pannaga-18 <pannaga.rj@gmail.com>
2024-12-04 07:39:54 +0530

    REsources, About, contact

---
+++ resources.js
@@ -0,0 +1,11 @@
+// // Interactivity for the resources page
+// document.addEventListener("DOMContentLoaded", () => {
+//     const buttons = document.querySelectorAll(".btn");
+
+//     buttons.forEach(button => {
+//         button.addEventListener("click", (event) => {
+//             event.preventDefault();
+//             alert(`Navigating to ${button.innerText}!`);
+//         });
+//     });
+// });
